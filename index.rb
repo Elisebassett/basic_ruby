ce0b4f018a110157e2cc58458672f020e0878c1d
Elisebassett <elise@iileaders.com>
2017-10-25 16:42:11 -0600

    fixes #6

--- index.rb
+++ index.rb
@@ -1,9 +1,11 @@
 def crazyCountry(phrase)
-	phrase + ' Only in America.'
+	puts phrase + ' Only in America.'
 end
 
 puts "What crazy thing has happened to you lately?"
 phrase = gets.chomp
+
+crazyCountry phrase
 
 number_array = [30, 50, 200, 5000, 20, 13, -5, -10, 0]
 
@@ -14,3 +16,17 @@ def biggestNumber(array)
 end
 
 puts biggestNumber number_array
+
+cheese_array = [:Rochefort, :Taleggio, :Manchego, :Kunik]
+country_array = ['France', 'Italy', 'Spain', 'America']
+
+def cheeseInfo(cheese_array, country_array)
+	cheese_info = {}
+	
+	for i in 0...cheese_array.length do
+		cheese_info[cheese_array[i]] = country_array[i]
+	end
+	puts cheese_info
+end
+
+cheeseInfo cheese_array, country_array
